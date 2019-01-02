420af2e5f65eaa94a7b4d2c3fae34889686c1fbf
Oussama <oussamamage16@hotmail.com>
2019-01-02 23:25:40 +0100

    Update fbCf.js

--- src/fbCf.js
+++ src/fbCf.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-var config = {
-    apiKey: "AIzaSyBCVwQX2Mb8GtWoIZjS2mBPGGYDKIwbwII",
-    authDomain: "clientpanel-def5b.firebaseapp.com",
-    databaseURL: "https://clientpanel-def5b.firebaseio.com",
-    projectId: "clientpanel-def5b",
-    storageBucket: "clientpanel-def5b.appspot.com",
-    messagingSenderId: "670793099388"
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
